4ca63a461c881c88ce07ab406196edba48750471
nava6x <navafacts2026@gmail.com>
2024-12-18 02:49:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://testprs.onrender.com",
     methods: ["GET", "POST"]
   }
 });
