7f90457f60f165ff81e810216b35a1e43afe7ddb
nava6x <navafacts2026@gmail.com>
2024-12-18 01:38:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 // Import the Express module
 const express = require('express');
 
+
 const Objects = {
   userId: 1,
   id: 1,
   title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
   body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
 }
+
 // Initialize the Express application
 const app = express();
 
-// Define a port for the server
-const PORT = 3000;
-
+const port = process.env.port || 500
 // Middleware to parse JSON bodies (optional)
 app.use(express.json());
 
@@ -26,6 +26,6 @@ app.get('/api', (req, res) => {
 })
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`running`);
 });
